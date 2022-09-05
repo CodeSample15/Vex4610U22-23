8aa92c592316195c82ef083978b0138faa1098e2
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2022-09-05 18:50:53 -0400

    Created new prototypes

--- include/UserDefined/functions.h
+++ include/UserDefined/functions.h
@@ -4,6 +4,7 @@
   * Project: PositionTracking
   * This will hold all of the functions and variables used by the main script
 */
+
 #pragma once
 #define PI 3.1415926
 
@@ -15,16 +16,16 @@ extern PID turnPid;
 extern PID movePid;
 
 //variables
-int robot_x;
-int robot_y;
+extern int robot_x;
+extern int robot_y;
 
-int lastEncoderPositionX;
-int lastEncoderPositionY;
+extern int lastEncoderPositionX;
+extern int lastEncoderPositionY;
 
 //milliseconds between each position update
-int PositionUpdateRate; //should NEVER be zero. Thread will take up entire processor usage and stop the code from running
+extern int PositionUpdateRate; //should NEVER be zero. Thread will take up entire processor usage and stop the code from running
 
-bool stopThreads;
+extern bool stopThreads;
 
 //functions used by the main file
 void init(); //set variable values and stuff
@@ -33,3 +34,9 @@ void update_pos(); //update position based off of how fast the wheels of the rob
 Points getPositionXY(); //returns the robots current position
 
 //movement functions
+void hardDriveStop();
+void Move(int amount, int speed, bool hardstop);
+void Move(PID& pid, int amount, double speed);
+void Move(PID& pid, PID& turnPID, int amount, double speed);
+
+//void MoveUntilLine(int speed); (maybe)
